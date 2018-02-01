71c675bf7380e5a0f6a68f7e1bef0060ce8d0892
petrlaptev <bastsoft@yandex.ru>
2018-02-01 12:27:29 +0500

    creationHighway

--- app/buildings.js
+++ app/buildings.js
@@ -49,6 +49,68 @@ const buildingByWay = function (way, world) {
     return ['a-entity', {}, elem];
 };
 
+const creationHighway = function (way, world) {
+    const elem = [];
+    const currentNd = way.nd;
+
+    let lastPoint = null;
+    Object.keys(currentNd).forEach(function (index) {
+        const ref = currentNd[index]._ref;
+        const currentPoint = world.nodesObj[ref].point;
+
+        if (lastPoint) {
+            const trueWidth = calculateLengthSegment(currentPoint.x, currentPoint.y, lastPoint.x, lastPoint.y);
+            const m1 = calculateMidPoint(currentPoint.x, lastPoint.x, currentPoint.y, lastPoint.y);
+
+            const ang = calculateAngleBetweenTwoStraight(
+                {X: m1.x, Y: m1.y},
+                {X: Number(lastPoint.x), Y: Number(lastPoint.y)},
+                {X: Number(m1.x + (trueWidth / 2)), Y: Number(m1.y)}
+            );
+
+            let height = 1;
+            let color = "red";
+
+            if(way.tag.highway === "footway"){
+                height = 4;
+                color = "orange"
+            }
+
+            if(way.tag.highway === "service"){
+                height = 8;
+                color = "gray"
+            }
+
+            if(way.tag.highway === "residential"){
+                height = 10;
+                color = "silver"
+            }
+
+            if(way.tag.highway === "secondary"){
+                height = 12;
+                color = "gainsboro"
+            }
+
+            if(color === "red"){
+                console.log(way.tag.highway);
+            }
+
+
+            elem.push(['a-plane', {
+                position: [m1.x, 0.01, m1.y].join(" "),
+                rotation: "-90 " + ang + " 0",
+                width: trueWidth,
+                height: height,
+                color: color,
+            }]);
+        }
+
+        lastPoint = currentPoint;
+    });
+
+    return ['a-entity', {}, elem];
+};
+
 const createBuildings = function (world) {
     const elem = [];
     Object.keys(world.relation).forEach(function (relationKey) {
@@ -70,12 +132,13 @@ const createBuildings = function (world) {
             way = null;
         }
 
-        if (way && tag.highway === "footway") {
-            //creationFootWay(way, world);
+        if (tag.highway) {
+            elem.push(creationHighway(way, world));
+            way = null;
         }
 
         if (way) {
-            console.log(way.tag);
+            //console.log(way.tag);
         }
     });
 
